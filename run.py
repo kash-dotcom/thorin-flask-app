c0f23b94c8ec4998207aeaa3c22132cea78d771a
Katy Ashworth <katyashworth27@gmail.com>
2024-12-30 22:00:32 +0000

    Use for loop to render About page with JSON data

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template
+import json
 
 
 app = Flask(__name__)
@@ -12,17 +13,20 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company=data)
 
 
 @app.route("/contact.html")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
